1287087e52480655c324dd56af2b0836eb3fa5f7
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-07-08 06:24:35 +0530

    Update paddle[1].py

--- paddle[1].py
+++ paddle[1].py
@@ -15,6 +15,7 @@ class Paddle(Turtle):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
+    
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
